76426bcf3dcdedb6f38657df5d0d52e28259b499
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-16 15:46:54 +0300

    create navbar component

---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className='bg-yellow-600 h-screen max-w-[10cm]'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
