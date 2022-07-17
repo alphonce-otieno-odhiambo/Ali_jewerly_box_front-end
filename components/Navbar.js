858d0adcd16a0198fb1ee7d5ad3df82754f2d23a
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-17 08:07:48 +0300

    install hero icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import titleice from "../public/titleicebox.jpeg"
 
 function Navbar() {
   return (
-    <div className='bg-yellow-600 h-screen max-w-[10cm]'>
+    <section className='bg-[#D8C3A5] h-screen max-w-[5cm] fixed top-0 '>
+        <Image
+                    src={titleice} alt='icebox'
+                    
+                    objectFit='contain'
+                    height={100}
+                    width={200}
+                    className="rounded-2xl mt-2 p-5"
+                    />
         <div>
-
+            
         </div>
-    </div>
+    </section>
   )
 }
 
