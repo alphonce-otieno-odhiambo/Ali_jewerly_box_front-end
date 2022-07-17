0ef0b785824cdbdf3df1427330fe516b20b27235
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-17 18:08:57 +0300

    install motion framer

--- components/Body.js
+++ components/Body.js
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+// import icevideo from "../public/assets/icebox.mp4"
 
 
 function Body() {
   return (
-    <section>
-     
+    <section className=''>
+       {/* Avatars scrolling arround showing images */}
+      <div>
+       
+        {/* burna video at the background */}
+        <video 
+          src={"../public/assets/icebox.mp4"}
+          autoPlay
+          loop
+          muted
+          height='100px'
+          width='200px'
+        />
+       
+      </div>
     </section>
   )
 }
--- components/SearchBar.js
+++ components/SearchBar.js
@@ -7,9 +7,10 @@ function SearchBar() {
   return (
     <div className='max-w-screen-md' >
         <div className='flex items-center mt-4  rounded-full  ' >
-            <SearchIcon className='h-6 w-6'/>
-            <input type="text" placeholder='Search for product mr-5'
-            className='text-[#8E8D8A] h-10 flex-grow rounded-full pl-2 bg-[#EAE7DC] border-none '
+            <SearchIcon className='h-6 w-6 rounded-full text-[#E98074]'/>
+            <input type="text" placeholder='Search for product'
+            className='text-[#E85A4F] h-10 flex-grow rounded-full pl-6  
+            bg-transparent outline-none text-lg font-mono '
             />
            
         </div>
