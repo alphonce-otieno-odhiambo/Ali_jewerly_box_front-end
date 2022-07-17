e12d96305b337003268a9f899f222b006e540b18
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-07-17 08:41:06 +0300

    import  navbar icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import Image from 'next/image';
 import titleice from "../public/titleicebox.jpeg"
+import {
+  HomeIcon,
+  ShoppingCartIcon,
+  
+} from "@heroicons/react/outline"
+import {
+  UserGroupIcon,
+} from "@heroicons/react/solid"
 
 function Navbar() {
   return (
-    <section className='bg-[#D8C3A5] h-screen max-w-[5cm] fixed top-0 '>
+    <section className='top-0 z-40 bg-[#D8C3A5] h-screen max-w-[3cm]  items-center space-y-4'>
         <Image
                     src={titleice} alt='icebox'
                     
                     objectFit='contain'
-                    height={100}
-                    width={200}
-                    className="rounded-2xl mt-2 p-5"
+                    height={50}
+                    width={50}
+                    className=" mt-2 p-2 rounded-full ml-4"
                     />
-        <div>
+        <div className='items-center ml-2 space-y-8'>
+          <div className='flex items-center space-x-1'>
+          <HomeIcon className='icon' />
+          <h3 className='text-lg font-semibold mt-2'>Home</h3>
+          </div>
             
+            <ShoppingCartIcon className='icon'/>
+            <UserGroupIcon className='icon'/>
         </div>
     </section>
   )
